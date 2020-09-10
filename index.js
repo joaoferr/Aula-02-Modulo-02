5d5885aeba86f2c9ce2d1103e1a32f990f92c276
joaoferr <joaoaugusto@gmail.com>
2020-09-10 12:46:15 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,64 @@
+import { promises } from "fs";
+import express from "express";
+import timesRouter from "./routes/times.js";
+
+const {readFile, writeFile} = promises;
+const times = [];
+
+init();
+
+const app = express();
+app.use(express.json());
+
+app.use("/times", timesRouter);
+
+app.listen(3000, () => {
+    console.log("API Started");
+});
+
+async function init() {
+    try {
+        const resp = await readFile("./2003.json");
+        const data = JSON.parse(resp);
+
+        //montando array de times
+        data[0].partidas.forEach(partida => {
+            times.push({ time: partida.mandante, pontuacao: 0 });
+            times.push({ time: partida.visitante, pontuacao: 0 });
+        });
+
+        //montando pontuacao dos times no array criado
+        data.forEach(rodada => {
+            rodada.partidas.forEach(partida => {
+                const indexMandante = times.findIndex(item => item.time === partida.mandante);
+                const indexVisitante = times.findIndex(item => item.time === partida.visitante);
+                let timeMandante = times[indexMandante];
+                let timeVisitante = times[indexVisitante];
+
+                if (partida.placar_visitante > partida.placar_mandante) {                                                            
+                    timeVisitante.pontuacao += 3;
+                    times[indexVisitante] = timeVisitante;
+                } else if (partida.placar_mandante > partida.placar_visitante) {
+                    timeMandante.pontuacao += 3;
+                    times[indexMandante] = timeMandante;
+                } else {
+                    timeMandante.pontuacao += 1;
+                    times[indexMandante] = timeMandante;               
+
+                    timeVisitante.pontuacao += 1;
+                    times[indexVisitante] = timeVisitante;
+                }
+            });
+        });
+
+        //ordenar times pela pontuacao
+        times.sort((a, b) => {
+            return b.pontuacao - a.pontuacao;
+        });
+
+        //salvando array de times ordenado pela pontuacao
+        await writeFile("times.json", JSON.stringify(times));
+    } catch (err) {
+        console.log(err);
+    }
+}
